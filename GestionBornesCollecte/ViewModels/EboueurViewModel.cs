04f75e1b8f35cd82fddacfa205ad10828baebe3e
rymdje <rym.djerourou@gmail.com>
2026-03-11 14:57:08 +0100

    vue eboueur commencer

---
+++ GestionBornesCollecte/ViewModels/EboueurViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using GestionBornesCollecte.Helpers;
+using GestionBornesCollecte.Models;
+using GestionBornesCollecte.Services;
+
+namespace GestionBornesCollecte.ViewModels
+{
+    public class EboueurViewModel : BaseViewModel
+    {
+        private readonly BorneService _service = new BorneService();
+
+        public ObservableCollection<Borne> Bornes { get; } = new ObservableCollection<Borne>();
+
+        private Borne? _selectedBorne;
+        public Borne? SelectedBorne
+        {
+            get => _selectedBorne;
+            set
+            {
+                _selectedBorne = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(AlerteVisible));
+            }
+        }
+
+        public Visibility AlerteVisible =>
+            _selectedBorne != null && _selectedBorne.NiveauRemplissage >= 85
+            ? Visibility.Visible
+            : Visibility.Collapsed;
+
+        public ICommand Top10Command { get; }
+        public ICommand ToutesCommand { get; }
+        public ICommand MarquerVideeCommand { get; }
+
+        public EboueurViewModel()
+        {
+            Top10Command = new RelayCommand(_ => AppliquerTop10());
+            ToutesCommand = new RelayCommand(_ => Recharger());
+            MarquerVideeCommand = new RelayCommand(_ => MarquerVidee());
+
+            Recharger();
+        }
+
+        private void Recharger()
+        {
+            var liste = _service.GetBornesAsync().Result;
+            Bornes.Clear();
+            foreach (var b in liste)
+                Bornes.Add(b);
+            SelectedBorne = Bornes.FirstOrDefault();
+        }
+
+        private void AppliquerTop10()
+        {
+            var triees = Bornes
+                .OrderByDescending(b => b.NiveauRemplissage)
+                .ToList();
+            Bornes.Clear();
+            foreach (var b in triees)
+                Bornes.Add(b);
+            SelectedBorne = Bornes.FirstOrDefault();
+        }
+
+        private void MarquerVidee()
+        {
+            if (SelectedBorne == null) return;
+            int idBorne = SelectedBorne.IdBorne;
+            SelectedBorne.NiveauRemplissage = 0;
+            var copie = Bornes.ToList();
+            Bornes.Clear();
+            foreach (var b in copie)
+                Bornes.Add(b);
+            SelectedBorne = Bornes.FirstOrDefault(b => b.IdBorne == idBorne);
+            OnPropertyChanged(nameof(AlerteVisible));
+        }
+    }
+}
--- GestionBornesCollecte/Views/EboueurWindow.xaml.cs
+++ GestionBornesCollecte/Views/EboueurWindow.xaml.cs
@@ -1,27 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace GestionBornesCollecte.Views
 {
-    /// <summary>
-    /// Logique d'interaction pour EboueurWindow.xaml
-    /// </summary>
     public partial class EboueurWindow : Window
     {
         public EboueurWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnRetour_Click(object sender, RoutedEventArgs e)
+        {
+            var login = new LoginWindow();
+            login.Show();
+            this.Close();
         }
     }
 }
